d0f7d537713ccedcab21aeda68b20b9ae6a29d8d
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2026-02-10 17:40:35 -0500

    Add all components

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -31,7 +31,7 @@ const StyledIcon = withUniwind(IconImpl, {
  * @example
  * ```tsx
  * import { ArrowRight } from 'lucide-react-native';
- * import { Icon } from '@/registry/components/ui/icon';
+ * import { Icon } from '@/registry/uniwind/registry/components/ui/icon';
  *
  * <Icon as={ArrowRight} className="text-red-500 size-4" />
  * ```
